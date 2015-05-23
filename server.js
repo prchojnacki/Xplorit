5ea1e39fc24218ff616cbaecd866c36fcb008fab
Christopher Clark <chrisdclark3@gmail.com>
2015-05-23 14:06:21 -0700

    Chris's first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var server = app.listen(8000, function() { console.log("Server on 8000..."); });
+var io = require('socket.io').listen(server);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
